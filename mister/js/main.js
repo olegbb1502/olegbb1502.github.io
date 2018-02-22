9198aefb0b81aa82a7ee79e88f810362e57d187b
olegbb1502 <bolobanoleg@gmail.com>
2018-02-22 21:13:00 +0200

    mister page
    mister page

---
+++ mister/js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+    var welcomeSect = $('.welcome-section'),
+        enterButton = welcomeSect.find('.enter-button'),
+        animation = $('.fly-in-text');
+    
+    setTimeout(function () {
+           welcomeSect.removeClass('content-hidden');
+           // animation.addClass('animation');
+    }, 800);
+
+    enterButton.on('click', function (e) {
+        e.preventDefault();
+        welcomeSect.addClass('content-hidden').fadeOut();
+    });
+});
+
+
+var deadline='2018-02-22 20:15:00';
+
+$('.countdown').downCount({
+        date: deadline,
+    },
+    function(){
+        // alert("Время истекло!");
+    });
+
+
+
+// $( ".glitch-img" ).mgGlitch();
