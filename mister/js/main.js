f12c123b26727a2fad3ebfb8552ec1c1dcd787d0
olegbb1502 <bolobanoleg@gmail.com>
2018-02-26 11:18:21 +0200

    0

--- mister/js/main.js
+++ mister/js/main.js
@@ -24,38 +24,19 @@ $('.countdown').downCount({
         // alert("Время истекло!");
     });
 
-var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
 
-function moveBackground() {
-    x += (lFollowX - x) * friction;
-    y += (lFollowY - y) * friction;
+$(document).ready(function() {
 
-    translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
-
-    $('.welcome-section').css({
-        '-webit-transform': translate,
-        '-moz-transform': translate,
-        'transform': translate
-    });
-
-    window.requestAnimationFrame(moveBackground);
-}
-
-$(window).on('mousemove click', function(e) {
-
-    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-    lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-    lFollowY = (10 * lMouseY) / 100;
+    $('a').hoverIntent(function() {
+            $(this).removeClass('active');
+            setTimeout(function(){
+                $(this).addClass('temp');
+            },500);
+        },
+        function() {
+            $(this).addClass('active');
+            $(this).removeClass('temp');
+        }
+    );
 
 });
-
-moveBackground();
-
-// $( ".countdown" ).mgGlitch({
-//     glitch: true
-// });
