71fb1e868022159fc43bf296c3fcaa599542778f
olegbb1502 <bolobanoleg@gmail.com>
2018-04-09 21:45:20 +0300

    21.45
    Chnge gallery and small bags

--- mister/js/main.js
+++ mister/js/main.js
@@ -314,15 +314,14 @@ $(function () {
                 menu.slideTop();
             }
         });
-
-        $(window).resize(function(){
             var windowWidth = $(window).width();
-            if(windowWidth <=800){
+            if(windowWidth <800){
+                console.log('win<800')
                 $('.underline').on('click', function () {
                     menu.slideToggle();
+                    console.log('click');
                 })
             }
-        });
 
 
         // subMenu.hover(function(){
@@ -332,12 +331,64 @@ $(function () {
         // });
     })(jQuery);
 
-    var quadimages = document.querySelectorAll("#quad figure");
-    for(i=0; i<quadimages.length; i++) {
-        quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") });
-    }
-
-
-
-
 });
+
+let galleryItem = document.getElementsByClassName("gallery-item");
+let lightBoxContainer = document.createElement("div");
+let lightBoxContent = document.createElement("div");
+let lightBoxImg = document.createElement("img");
+let lightBoxPrev = document.createElement("div");
+let lightBoxNext = document.createElement("div");
+
+lightBoxContainer.classList.add("lightbox");
+lightBoxContent.classList.add("lightbox-content");
+lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
+lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
+
+lightBoxContainer.appendChild(lightBoxContent);
+lightBoxContent.appendChild(lightBoxImg);
+lightBoxContent.appendChild(lightBoxPrev);
+lightBoxContent.appendChild(lightBoxNext);
+
+document.body.appendChild(lightBoxContainer);
+
+let index = 1;
+
+function showLightBox(n) {
+    if (n > galleryItem.length) {
+        index = 1;
+    } else if (n < 1) {
+        index = galleryItem.length;
+    }
+    let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
+    lightBoxImg.setAttribute("src", imageLocation);
+}
+
+function currentImage() {
+    lightBoxContainer.style.display = "block";
+
+    let imageIndex = parseInt(this.getAttribute("data-index"));
+    showLightBox(index = imageIndex);
+}
+for (let i = 0; i < galleryItem.length; i++) {
+    galleryItem[i].addEventListener("click", currentImage);
+}
+
+function slideImage(n) {
+    showLightBox(index += n);
+}
+function prevImage() {
+    slideImage(-1);
+}
+function nextImage() {
+    slideImage(1);
+}
+lightBoxPrev.addEventListener("click", prevImage);
+lightBoxNext.addEventListener("click", nextImage);
+
+function closeLightBox() {
+    if (this === event.target) {
+        lightBoxContainer.style.display = "none";
+    }
+}
+lightBoxContainer.addEventListener("click", closeLightBox);
