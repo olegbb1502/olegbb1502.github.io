6b3683c3f94c83ee7409092188d4ad3730551360
olegbb1502 <bolobanoleg@gmail.com>
2019-04-06 10:24:15 +0300

    panel

---
+++ pitbull-admin/precache-manifest.61cfe109a00b432d61a5170b03bada2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6fdb154e57af6aa7661",
+    "url": "/static/js/main.46c895e9.chunk.js"
+  },
+  {
+    "revision": "6d5b0caf225ceaf3e0e4",
+    "url": "/static/js/2.84c3f96a.chunk.js"
+  },
+  {
+    "revision": "c6fdb154e57af6aa7661",
+    "url": "/static/css/main.eedb6b28.chunk.css"
+  },
+  {
+    "revision": "f3ff002dca2553daa08dbe29011263db",
+    "url": "/index.html"
+  }
+];
