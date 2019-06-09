032886ee27540702e5fc44c1edcbc077901abfd9
olegbb1502 <bolobanoleg@gmail.com>
2019-06-09 13:21:18 +0300

    new commit

---
+++ heart-monitor/precache-manifest.3026d0210d3ddab0497e7dafce198732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c748dacbc7e79de1cf0d875a34d0c9d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a83193f533446477ef3",
+    "url": "/static/css/2.714b1602.chunk.css"
+  },
+  {
+    "revision": "d84b02d02398b7d624eb",
+    "url": "/static/css/main.5fd7aad8.chunk.css"
+  },
+  {
+    "revision": "3a83193f533446477ef3",
+    "url": "/static/js/2.3e1be7ea.chunk.js"
+  },
+  {
+    "revision": "d84b02d02398b7d624eb",
+    "url": "/static/js/main.3d21f348.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
